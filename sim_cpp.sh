0cee8b618a77d8f20aa3feeaa99c0fd0b830bfba
ghj1222 <1342330485@qq.com>
2025-03-27 08:40:15 +0800

    fix shell

--- sim_cpp.sh
+++ sim_cpp.sh
@@ -3,7 +3,7 @@ if ! [ -d "build" ]; then
     mkdir build
 fi
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-8/root/usr/bin/g++
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
 for i in {1..16} ; do
--- trace_gen_sample.sh
+++ trace_gen_sample.sh
@@ -3,6 +3,6 @@ if ! [ -d "build" ]; then
     mkdir build
 fi
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-8/root/usr/bin/g++
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 ./src/trace_gen ../../configs/trace_config_sample.json ../../traces-sample/trace
